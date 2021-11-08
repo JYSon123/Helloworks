fcbc13d29d193c8325dc9abea0056440fb659981
JuYeonson <jubar22222@gmail.com>
2021-11-08 14:20:22 +0900

    인사 메뉴용 패키지 생성

---
+++ HelloWorks/src/main/java/com/spring/employees/controller/EmpController_jy.java
@@ -0,0 +1,25 @@
+package com.spring.employees.controller;
+
+import java.util.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.employees.service.InterEmpService_jy;
+
+@Controller
+public class EmpController_jy {
+	
+	
+	@Autowired  // 의존객체 설정
+	private InterEmpService_jy service;
+	
+
+	
+	
+}
